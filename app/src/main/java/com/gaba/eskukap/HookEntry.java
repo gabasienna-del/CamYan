c358305ddc4fbb97cce6deab4419ed38f763b409
ВашеИмя <you@example.com>
2025-12-06 11:48:09 +0500

    add camera2 hook

--- app/src/main/java/com/gaba/eskukap/HookEntry.java
+++ app/src/main/java/com/gaba/eskukap/HookEntry.java
@@ -1,41 +1,45 @@
 package com.gaba.eskukap;
 
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraDevice;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-/**
- * Простой тестовый хук:
- * - логируем загрузку ru.yandex.taximeter
- * - хукаем String.toString() только для проверки работы модуля.
- */
 public class HookEntry implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
 
-        if (!"ru.yandex.taximeter".equals(lpparam.packageName)) {
-            return;
-        }
+        if (!"ru.yandex.taximeter".equals(lpparam.packageName)) return;
 
-        XposedBridge.log("EskukapHook: handleLoadPackage for " + lpparam.packageName);
+        XposedBridge.log("EskukapHook: Loaded taximeter");
 
+        // Хук открытия камеры
         try {
             XposedHelpers.findAndHookMethod(
-                    "java.lang.String",
-                    lpparam.classLoader,
-                    "toString",
+                    CameraManager.class,
+                    "openCamera",
+                    String.class,
+                    CameraDevice.StateCallback.class,
+                    android.os.Handler.class,
                     new XC_MethodHook() {
                         @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            XposedBridge.log("EskukapHook: Camera open request -> " + param.args[0]);
+                        }
+
+                        @Override
                         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            XposedBridge.log("EskukapHook: String.toString() called");
+                            XposedBridge.log("EskukapHook: Camera opened");
                         }
                     }
             );
-        } catch (Throwable t) {
-            XposedBridge.log("EskukapHook ERROR in hook: " + t);
+        } catch (Throwable e) {
+            XposedBridge.log("EskukapHook CAMERA2 HOOK ERROR: " + e);
         }
     }
 }
