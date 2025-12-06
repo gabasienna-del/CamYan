26f9702193d325c16f49044aba3802723efde1a3
ВашеИмя <you@example.com>
2025-12-06 20:20:54 +0500

    update HookEntry + logs

--- app/src/main/java/com/gaba/eskukap/HookEntry.java
+++ app/src/main/java/com/gaba/eskukap/HookEntry.java
@@ -1,9 +1,10 @@
 package com.gaba.eskukap;
 
-import android.graphics.Bitmap;
-import android.graphics.SurfaceTexture;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.TextureView;
-import android.view.TextureView.SurfaceTextureListener;
+import android.graphics.SurfaceTexture;
+import android.graphics.Bitmap;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -16,33 +17,18 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class HookEntry implements IXposedHookLoadPackage {
 
-    private static void saveFrame(TextureView tv) {
+    private static void scanViews(View root) {
         try {
-            Bitmap bmp = tv.getBitmap();
-            if (bmp == null) {
-                XposedBridge.log("Eskukap CAM: bitmap null");
-                return;
+            if (root instanceof TextureView) {
+                XposedBridge.log("Eskukap: TextureView FOUND -> " + root.getClass().getName());
             }
-
-            File baseDir = tv.getContext().getExternalFilesDir(null);
-            if (baseDir == null) {
-                XposedBridge.log("Eskukap CAM: baseDir null");
-                return;
+            if (root instanceof ViewGroup) {
+                ViewGroup vg = (ViewGroup) root;
+                for (int i = 0; i < vg.getChildCount(); i++) {
+                    scanViews(vg.getChildAt(i));
+                }
             }
-
-            File dir = new File(baseDir, "Eskukap_cam");
-            //noinspection ResultOfMethodCallIgnored
-            dir.mkdirs();
-
-            File file = new File(dir, "cam_" + System.currentTimeMillis() + ".jpg");
-            FileOutputStream fos = new FileOutputStream(file);
-            bmp.compress(Bitmap.CompressFormat.JPEG, 80, fos);
-            fos.close();
-
-            XposedBridge.log("Eskukap CAM SAVED -> " + file.getAbsolutePath());
-        } catch (Throwable e) {
-            XposedBridge.log("Eskukap CAM save error: " + e);
-        }
+        } catch (Throwable ignored) {}
     }
 
     @Override
@@ -50,76 +36,18 @@ public class HookEntry implements IXposedHookLoadPackage {
 
         if (!lpparam.packageName.equals("ru.yandex.taximeter")) return;
 
-        XposedBridge.log("Eskukap: hook setSurfaceTextureListener");
+        XposedBridge.log("Eskukap: hook active - scanning layouts");
 
-        Class<?> textureViewClass = XposedHelpers.findClass(
-                "android.view.TextureView",
-                lpparam.classLoader
-        );
-
-        // Хукаем установку слушателя
         XposedHelpers.findAndHookMethod(
-                textureViewClass,
-                "setSurfaceTextureListener",
-                SurfaceTextureListener.class,
+                "android.view.LayoutInflater",
+                lpparam.classLoader,
+                "inflate",
+                int.class, ViewGroup.class, boolean.class,
                 new XC_MethodHook() {
                     @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-
-                        final TextureView tv = (TextureView) param.thisObject;
-                        final Object listener = param.args[0];
-
-                        if (listener == null) {
-                            XposedBridge.log("Eskukap CAM: listener null");
-                            return;
-                        }
-
-                        Class<?> listenerClass = listener.getClass();
-                        XposedBridge.log("Eskukap CAM: listener class = " + listenerClass.getName());
-
-                        try {
-                            // Хук onSurfaceTextureAvailable конкретного класса
-                            XposedHelpers.findAndHookMethod(
-                                    listenerClass,
-                                    "onSurfaceTextureAvailable",
-                                    SurfaceTexture.class, int.class, int.class,
-                                    new XC_MethodHook() {
-                                        @Override
-                                        protected void afterHookedMethod(MethodHookParam param2) throws Throwable {
-                                            XposedBridge.log("Eskukap CAM: onSurfaceTextureAvailable");
-                                            saveFrame(tv); // один кадр при старте
-                                        }
-                                    }
-                            );
-                        } catch (Throwable e) {
-                            XposedBridge.log("Eskukap CAM: hook onSurfaceTextureAvailable error " + e);
-                        }
-
-                        try {
-                            // Дополнительно хук onSurfaceTextureUpdated для кадров в процессе
-                            XposedHelpers.findAndHookMethod(
-                                    listenerClass,
-                                    "onSurfaceTextureUpdated",
-                                    SurfaceTexture.class,
-                                    new XC_MethodHook() {
-                                        @Override
-                                        protected void afterHookedMethod(MethodHookParam param3) throws Throwable {
-                                            // Можно сильно заспамить, поэтому пока делаем 1 кадр раз в несколько вызовов
-                                            Object counterObj = XposedHelpers.getAdditionalInstanceField(tv, "eskukap_counter");
-                                            int counter = (counterObj instanceof Integer) ? (Integer) counterObj : 0;
-                                            counter++;
-                                            if (counter >= 15) { // примерно каждый 15-й кадр
-                                                counter = 0;
-                                                XposedBridge.log("Eskukap CAM: onSurfaceTextureUpdated -> save");
-                                                saveFrame(tv);
-                                            }
-                                            XposedHelpers.setAdditionalInstanceField(tv, "eskukap_counter", counter);
-                                        }
-                                    }
-                            );
-                        } catch (Throwable e) {
-                            XposedBridge.log("Eskukap CAM: hook onSurfaceTextureUpdated error " + e);
-                        }
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        View v = (View) param.getResult();
+                        if (v != null) scanViews(v);
                     }
                 }
         );
