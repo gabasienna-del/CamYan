b9bb1cc45ec652653f10e99876e83d9536cb2a5b
ВашеИмя <you@example.com>
2025-12-06 11:55:20 +0500

    add frame hook

--- app/src/main/java/com/gaba/eskukap/HookEntry.java
+++ app/src/main/java/com/gaba/eskukap/HookEntry.java
@@ -2,6 +2,8 @@ package com.gaba.eskukap;
 
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraDevice;
+import android.media.ImageReader;
+import android.media.Image;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -18,7 +20,7 @@ public class HookEntry implements IXposedHookLoadPackage {
 
         XposedBridge.log("EskukapHook: Loaded taximeter");
 
-        // Хук открытия камеры
+        // ---- Хук открытия камеры ----
         try {
             XposedHelpers.findAndHookMethod(
                     CameraManager.class,
@@ -28,18 +30,43 @@ public class HookEntry implements IXposedHookLoadPackage {
                     android.os.Handler.class,
                     new XC_MethodHook() {
                         @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        protected void beforeHookedMethod(MethodHookParam param) {
                             XposedBridge.log("EskukapHook: Camera open request -> " + param.args[0]);
                         }
-
                         @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        protected void afterHookedMethod(MethodHookParam param) {
                             XposedBridge.log("EskukapHook: Camera opened");
                         }
                     }
             );
-        } catch (Throwable e) {
-            XposedBridge.log("EskukapHook CAMERA2 HOOK ERROR: " + e);
-        }
+        } catch (Throwable e) { XposedBridge.log("EskukapHook CAMERA2 HOOK ERROR " + e); }
+
+        // ---- Перехват ImageReader (видео кадры) ----
+        try {
+            XposedHelpers.findAndHookMethod(
+                ImageReader.class,
+                "onImageAvailable",
+                ImageReader.OnImageAvailableListener.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("EskukapHook: Frame incoming...");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        ImageReader reader = (ImageReader) param.thisObject;
+                        Image image = reader.acquireLatestImage();
+
+                        if (image != null) {
+                            XposedBridge.log("EskukapHook: Got frame -> " + image.getWidth() + "x" + image.getHeight());
+
+                            // ❗ здесь дальше будет подмена изображения
+                            image.close(); // временно закрываем кадр
+                        }
+                    }
+                }
+            );
+        } catch (Throwable e) { XposedBridge.log("EskukapHook FRAME HOOK ERROR " + e); }
     }
 }
